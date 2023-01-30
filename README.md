7d8289cc7c81ec27dd9b2641dba2bc21fd213a6b
Jonathan Chiang <jchiangconan@gmail.com>
2023-01-29 21:07:31 -0500

    too cool for school

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Syzygy Generative Pre-trained Transformer
 
 This was created following Andrej Karpathy's "Let's build GPT: from scratch, in code, spelled out." tutorial. Link: https://www.youtube.com/watch?v=kCc8FmEb1nY
+
+Running this project on Python 3.9 is preferred because of the support for `list` being used as a generic type.
--- train.py
+++ train.py
@@ -1,12 +1,23 @@
 import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+# hyperparameters
+BATCH_SIZE = 32
+BLOCK_SIZE = 8
+MAX_ITERS = 10000
+EVAL_INTERVAL = 1000
+LEARNING_RATe = 1e-2
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+EVAL_ITERS = 200
+
+torch.manual_seed(1337)
 
 with open("./corpora/tinyshakespeare.txt", "r", encoding="utf-8") as f:
     text = f.read()
 
-# print(text[:1000])
-
 chars = sorted(list(set(text)))
-print("".join(chars))
+vocab_size = len(chars)
 
 # tokenizer: create a mapping between characters and integers
 stoi = {ch: i for i, ch in enumerate(chars)}
@@ -15,10 +26,94 @@ def encode(s: str) -> list[int]: return [stoi[c] for c in s]
 def decode(k: list[int]) -> str: return "".join([itos[i] for i in k])
 
 
-print(encode("hi there"))
-print(decode(encode("hi there")))
-
+# train and test splits
 data = torch.tensor(encode(text), dtype=torch.long)
-print(data.shape, data.dtype)
-print(data[:1000])
+n = int(0.9 * len(data))
+train_data = data[:n]
+val_data = data[n:]
 
+
+# data loading
+def get_batch(split):
+    # generate a small batch of data of inputs x and targets y
+    data = train_data if split == "train" else val_data
+    ix = torch.randint(len(data) - BLOCK_SIZE, (BATCH_SIZE,))
+    x = torch.stack([data[i:i + BLOCK_SIZE] for i in ix])
+    y = torch.stack([data[i + 1:i + BLOCK_SIZE + 1] for i in ix])
+    x, y = x.to(DEVICE), y.to(DEVICE)
+    return x, y
+
+
+@torch.no_grad()
+def estimate_loss():
+    out = {}
+    model.eval()
+    for split in ["train", "val"]:
+        losses = torch.zeros(EVAL_ITERS)
+        for k in range(EVAL_ITERS):
+            X, Y = get_batch(split)
+            logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
+
+
+class BigramLM(nn.Module):
+    def __init__(self, _vocab_size):
+        super().__init__()
+        # each token directly reads off the logits for the next token from a lookup table
+        self.token_embedding_table = nn.Embedding(_vocab_size, _vocab_size)
+
+    def forward(self, idx, targets=None):
+        # idx and targets are both (B, T) tensors of integers
+        logits = self.token_embedding_table(idx) # (B, T, C) batch, time, channel
+        if targets is None:
+            loss = None
+        else:
+            b, t, c = logits.shape
+            logits = logits.view(b * t, c)
+            targets = targets.view(b * t)
+            loss = F.cross_entropy(logits, targets)
+
+        return logits, loss
+
+    def generate(self, idx, max_new_tokens):
+        # idx is (B, T) array of indices in the current context
+        for _ in range(max_new_tokens):
+            # get the prediction
+            logits, loss = self(idx)
+            # focus only on the last step
+            logits = logits[:, -1, :]  # becomes (B, C)
+            # apply softmax to get probabilities
+            probs = F.softmax(logits, dim=-1)  # (B, C)
+            # sample from the distribution
+            idx_next=torch.multinomial(probs, num_samples=1)  # (B, 1)
+            idx = torch.cat((idx, idx_next), dim=1)  # (B, T + 1)
+        return idx
+
+
+model = BigramLM(vocab_size)
+m = model.to(DEVICE)
+
+# create a PyTorch optimizer
+optimizer = torch.optim.AdamW(m.parameters(), lr=1e-3)
+
+for iteration in range(MAX_ITERS):
+    # every once in a while evaluate the loss on train and val sets
+    if iteration % EVAL_INTERVAL == 0:
+        losses = estimate_loss()
+        print(f"step {iteration}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+
+    # sample a batch of data
+    xb, yb = get_batch("train")
+
+    # evaluate the loss
+    logits, loss = model(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+
+# generate from the model
+context = torch.zeros((1, 1), dtype=torch.long, device=DEVICE)
+print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
